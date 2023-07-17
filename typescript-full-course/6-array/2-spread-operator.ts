d8c41fd42576234d7acd2f495f1bf89713bf3e44
a-ryang <chnwu.kim@gmail.com>
2023-07-17 21:12:48 +0900

    array

---
+++ typescript-full-course/6-array/2-spread-operator.ts
@@ -0,0 +1,3 @@
+const onlyString = ["1", "2", "3"];
+
+const arr = [...onlyString]; // string[]
---
+++ typescript-full-course/6-array/3-multi-dimension-array.ts
@@ -0,0 +1,11 @@
+const number2Darr: number[][] = [
+  [1, 2, 3],
+  [4, 5, 6],
+];
+
+for (let arr of number2Darr) {
+  // arr : number[]
+  for (let item of arr) {
+    // item: number
+  }
+}
